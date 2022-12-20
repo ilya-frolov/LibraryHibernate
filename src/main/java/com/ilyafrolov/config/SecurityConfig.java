4c341fc6fc7784369de56a591d88216d38b88f1e
ilya-frolov <ilyafrolov0711@gmail.com>
2022-12-20 13:38:09 +0200

    Library Web Securtiy: Login form works but not all jsps are authorized.

--- src/main/java/com/ilyafrolov/config/SecurityConfig.java
+++ src/main/java/com/ilyafrolov/config/SecurityConfig.java
@@ -1,46 +1,59 @@
-//package com.ilyafrolov.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
-//
+package com.ilyafrolov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+
 //    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("user")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("admin")
-//                .roles("ADMIN", "USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
 //    }
-//
-//    public SecurityFilterChain createChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.authorizeHttpRequests()
-//                .requestMatchers(HttpMethod.POST, "/user")
-//                .hasRole("ADMIN")
-////                .anyRequest()
-////                .authenticated()
-//                .and()
-//                .httpBasic();
-//
-//        return httpSecurity.build();
-//    }
-//}
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain createChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/user")
+                .hasRole("USER")
+                .requestMatchers(HttpMethod.GET, "/user-or-admin")
+                .hasAnyRole("USER", "ADMIN")
+//                .anyRequest()
+//                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+        return httpSecurity.build();
+    }
+}
--- src/main/java/com/ilyafrolov/controllers/StartController.java
+++ src/main/java/com/ilyafrolov/controllers/StartController.java
@@ -2,6 +2,8 @@ package com.ilyafrolov.controllers;
 
 import com.ilyafrolov.model.Book;
 import com.ilyafrolov.model.User;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,24 +12,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class StartController {
 
-    @RequestMapping(method = RequestMethod.GET, value="/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String start() {
         return "start";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/user")
     public String getUser(Model model, User user) {
-        model.addAttribute("username", user.getName());
+        SecurityContext context = SecurityContextHolder.getContext();
+        model.addAttribute("username", ((org.springframework.security.core.userdetails.User)context.getAuthentication().getPrincipal()).getUsername());
         model.addAttribute("age", user.getAge());
         return "user-info";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value="/book-start")
+    @RequestMapping(method = RequestMethod.GET, value = "/book-start")
     public String addBook() {
         return "book-start";
     }
 
-    @RequestMapping(method=RequestMethod.POST, value="/book")
+    @RequestMapping(method = RequestMethod.POST, value = "/book")
     public String getBook(Model model, Book book) {
         model.addAttribute("title", book.getTitle());
         model.addAttribute("author", book.getAuthor().getName());
@@ -35,5 +38,13 @@ public class StartController {
         return "book-info";
     }
 
-
+    @RequestMapping(method = RequestMethod.GET, value = "/user-or-admin")
+    public String getUserAdmin(Model model, User user) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        model.addAttribute("username", ((org.springframework.security.core.userdetails.User)context.getAuthentication().getPrincipal()).getUsername());
+        model.addAttribute("age", 34);
+        return "user-info";
+    }
 }
+
+
